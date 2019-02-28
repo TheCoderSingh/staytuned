505c99331f970aa90c5bec34bfce5f16bfe21de3
Jaskaran Singh <jaskaran1523@gmail.com>
2019-02-28 16:40:18 +0530

    feat: Complete news fetching and add to the template

--- src/app/components/headlines/headlines.component.ts
+++ src/app/components/headlines/headlines.component.ts
@@ -1,15 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { HeadlinesfetcherService } from "./../../services/headlinesfetcher.service";
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-headlines',
-  templateUrl: './headlines.component.html',
-  styleUrls: ['./headlines.component.css']
+    selector: "app-headlines",
+    templateUrl: "./headlines.component.html",
+    styleUrls: ["./headlines.component.css"]
 })
 export class HeadlinesComponent implements OnInit {
+    news;
+    // headines;
+    constructor(private headlinesfetcherservice: HeadlinesfetcherService) {}
 
-  constructor() { }
+    async ngOnInit() {
+        await this.getHeadlines();
+    }
 
-  ngOnInit() {
-  }
-
+    async getHeadlines() {
+        this.news = await this.headlinesfetcherservice.fetchHeadlines();
+        // console.log(this.headlines["articles"]["0"]);
+        // for (let i = 0; i < 10; i++) {
+        // console.log(this.news["articles"][i.toString()]["title"]);
+        // this.headlines += this.news["articles"][i.toString()]["title"];
+        // }
+        // console.log(this.news["title"]);
+    }
 }
--- src/app/services/headlinesfetcher.service.ts
+++ src/app/services/headlinesfetcher.service.ts
@@ -1,9 +1,21 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: "root"
 })
 export class HeadlinesfetcherService {
+    result;
+    headlines;
+    API_KEY = "bdef3ef2d2aa4d2590035954dab74b69";
+    baseurl = "https://newsapi.org/v2/top-headlines?country=";
 
-  constructor() { }
+    constructor(private http: HttpClient) {}
+
+    async fetchHeadlines(): Promise<any> {
+        this.result = await this.http
+            .get(this.baseurl + "ca" + "&apiKey=" + this.API_KEY)
+            .toPromise();
+        return this.result["articles"];
+    }
 }
